08bdf5dcb120f4414f4cc863720242bfcbf78c19
cody11827 <codytien1@gmail.com>
2026-09-24 08:55:01 -0700

    add hover effect and add settings button

--- script.js
+++ script.js
@@ -2,10 +2,52 @@
 let container = document.querySelector(".container");
 
 
-for(let i =0; i < 256; i++){
-    let div = document.createElement("div");
-    div.innerHTML = "test";
-    div.classList.add('grid-square')
-    container.appendChild(div);
+function createGrid(){
+    for(let i =0; i < 256; i++){
+        let div = document.createElement("div");
+        div.innerHTML = "test";
+        div.classList.add('grid-square')
+        container.appendChild(div);
+    }
 }
+
+function onMouseOver(e){
+    let target = e.target;
+    console.log(target);
+
+    if (!target.classList.contains('grid-square')){
+        return;
+    }
+
+    target.style.backgroundColor = "red";
+}
+
+function onMouseOut(e){
+    let target = e.target;
+    console.log(target);
+
+    if (!target.classList.contains('grid-square')){
+        return;
+    }
+
+    target.style.backgroundColor = "white";
+}
+
+
+function onSettingsButtonClick(){
+    let gridSize = Math.min(100,prompt("Enter Grid Size: ",16))
+
+}
+
+
+function main(){
+    createGrid();
+    container.addEventListener('mouseover',onMouseOver);
+    container.addEventListener('mouseout',onMouseOut);
+    document.querySelector("button").addEventListener('click',onSettingsButtonClick);
+}
+
+
+main();
+
 
